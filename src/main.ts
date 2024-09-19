dfa6272d7eceec5c35521706aa68d5fea1a2aaf7
Danish <danishsidd524@gmail.com>
2024-09-19 21:11:27 +0500

    inital setup

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { HttpExceptionFilter } from './filters/http-client-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+
+  app.setGlobalPrefix('api/v1');
+  // app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: '*',
+    credentials: true,
+    allowedHeaders: '*',
+  });
+
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(port);
+}
+
+bootstrap();
